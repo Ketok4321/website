230919e96d189a6c235d89210f01472e46e4dc4b
Ketok <ketok4321@gmail.com>
2023-02-22 08:57:20 +0100

    Change code css a bit

--- src/lib/components/Code.svelte
+++ src/lib/components/Code.svelte
@@ -1,11 +1,10 @@
-<code>
-    <slot/>
-</code>
+<pre><code><slot/></code></pre>
 
 <style>
     code {
+        max-width: 100%;
         font-size: 1rem;
-        padding: 0.5em;
+        padding: 0.25em;
         background-color: rgb(32, 32, 32);
         color: var(--color-text-light);
         border: 0.2em solid rgb(16, 16, 16);
