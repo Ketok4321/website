c355cf840180ea4f97b56104c3175e45f1d45a14
Ketok <ketok4321@gmail.com>
2023-02-23 23:20:00 +0100

    Improve Box component

--- src/lib/components/Box.svelte
+++ src/lib/components/Box.svelte
@@ -1,30 +1,43 @@
-<div>
-    <slot/>
+<div class="box">
+    <div class="contents">
+        <slot/>
+    </div>
 </div>
 
 <style>
-    div {
+    .box {
         width: 100%;
+        margin: 10vh 0;
+        padding: 1em 0;
 
         background-color: var(--color-background-75);
         backdrop-filter: blur(0.5rem);
         border-top: 0.5rem solid var(--color-background-100);
         border-bottom: 0.5rem solid var(--color-background-100);
+    }
+
+    .contents {
+        width: 90%;
+        max-width: 40em;
+        margin: 0 auto;
 
         text-align: center;
         color: var(--color-text-light);
-        margin: 10vh 0;
-        padding: 1em 0;
     }
 
-    div :global(h1) {
-        font-size: 1.5em;
+    .contents > :global(:first-child) {
         margin-top: 0;
     }
 
-    div :global(p) {
-        width: 100%;
-        max-width: 40em;
+    .contents > :global(:last-child) {
+        margin-bottom: 0;
+    }
+
+    .box :global(h1) {
+        font-size: 1.5em;
+    }
+
+    .box :global(p) {
         margin: 1em auto;
         font-size: 0.75em;
     }
