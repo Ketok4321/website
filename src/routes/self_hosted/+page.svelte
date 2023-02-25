3b27a7123b951e273a7c46e48f30c1ed9eaad459
Ketok <ketok4321@gmail.com>
2023-02-25 15:37:05 +0100

    Fix remote add command

--- src/routes/self_hosted/+page.svelte
+++ src/routes/self_hosted/+page.svelte
@@ -12,7 +12,7 @@
 	<h1>Flatpak remote (repo)</h1>
 	<p>I host my own flatpak remote. I only created it for fun and learning purposes, there are no useful things downloadable from there.</p>
 	<p>Add the remote to your <del>system</del> user:</p>
-	<Code>flatpak remote-add --user gowno http://gowno.ketok.xyz/gowno.flatpakrepo</Code>
+	<Code>flatpak remote-add --user --no-gpg-verify gowno http://gowno.ketok.xyz/gowno.flatpakrepo</Code>
 	<p>List packages:</p>
 	<Code>flatpak remote-ls gowno</Code>
 </Box>
