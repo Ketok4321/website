a317637f9cae28389256c9c072c2f4193e1b92f5
Ketok <ketok4321@gmail.com>
2023-04-03 23:35:24 +0200

    Projects page (almost) finished

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let title;
+    export let subtitle = "";
+</script>
+
+<h1 style:margin-bottom="0">{title}</h1>
+<p style:margin-top="0">{subtitle}</p>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,6 +2,7 @@
     import Box from "$lib/components/Box.svelte"
 	import Link from "$lib/components/Link.svelte"
 	import Flex from "$lib/components/Flex.svelte"
+    import Title from "$lib/components/Title.svelte"
 </script>
 
 <Box>
@@ -10,7 +11,7 @@
 </Box>
 
 <Box>
-	<h1>Where Esolang</h1>
+	<Title title="Where Esolang" subtitle="Done" />
 	<p>
 		An esolang where memory is accessible only by using "WHERE" statement.
 		For example WHERE &lt;2 SET 0 sets all memory cells with value lower than 2 to 0.
@@ -22,5 +23,29 @@
 </Box>
 
 <Box>
-	<p>WIP</p>
+	<Title title="Advanced Esolang" subtitle="Work in progress" />
+	<p>
+		An object oriented esolang. The name comes from the fact that it has no primitives.
+	</p>
+	<Flex type="horizontal">
+		<Link href="https://github.com/Ketok4321/AdvancedEsolang" label="Repo" icon="github/f0f0f0" alt="github"/>
+	</Flex>
+</Box>
+
+<Box>
+	<Title title="Wilderness" subtitle="Development paused" />
+	<p>
+		A minecraft mod that enhances minecraft's forests by adding new, wilder variants of them, like old growth forest, or mixed forest.
+	</p>
+	<Flex type="horizontal">
+		<Link href="https://github.com/Ketok4321/wilderness" label="Repo" icon="github/f0f0f0" alt="github"/>
+	</Flex>
+</Box>
+
+<Box>
+	<Title title="The end" subtitle="That's all" />
+	<p>
+		Well, I have also created a few other small things, and made some minor contributions to other projects, but I don't think they are that noteworthy.
+		If you want to see them, you can check my <a href="https://github.com/Ketok4321" style:text-decoration="underline">GitHub</a> profile.
+	</p>
 </Box>
