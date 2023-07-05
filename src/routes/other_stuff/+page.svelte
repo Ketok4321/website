f38c14941dd0425d63073b6f1448738bcdbbf7c7
Ketok <ketok4321@gmail.com>
2023-07-05 13:53:52 +0200

    Update links to websites

--- src/routes/other_stuff/+page.svelte
+++ src/routes/other_stuff/+page.svelte
@@ -13,8 +13,8 @@
 <Box>
 	<Title title="Cool websites" subtitle="(by cool i mean those of my friends)"/>
 	<Flex type="horizontal">
-        <Friend name="Henior" href="https://henior.kefir.ovh" img="https://henior.kefir.ovh/assets/favicon.ico"/>
-        <Friend name="Musaszi" href="https://musaszi7.kefir.ovh" img="https://avatars.akamai.steamstatic.com/6c01eeb1a8ee2bad8bedfe051d3c77aa5f8556ba_full.jpg"/>
+        <Friend name="Henior" href="https://kefir.ovh" img="https://kefir.ovh/assets/favicon.ico"/>
+        <Friend name="Musaszi" href="https://musaszi7.jest.gay" img="https://avatars.akamai.steamstatic.com/6c01eeb1a8ee2bad8bedfe051d3c77aa5f8556ba_full.jpg"/>
         <Friend name="Xila" href="https://xina.pl" img="http://xina.pl/assets/img/avatar.png"/>
     </Flex>
 </Box>
